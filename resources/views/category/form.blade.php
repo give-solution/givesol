ed4a2d0cff69ad84c1c498f91ff76ee129065f5c
sehia99 <budifitt@gmail.com>
2018-04-03 00:04:27 +0700

    givesol

--- resources/views/category/form.blade.php
+++ resources/views/category/form.blade.php
@@ -3,7 +3,7 @@
 <div class="">
             <div class="page-title">
               <div class="title_left">
-                <h3>Add Category</h3>
+                <h3></h3>
               </div>
 
               </div>
@@ -12,7 +12,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Form Design <small>different form elements</small></h2>
+                    <h2>Add Category </h2>
                     
                     <div class="clearfix"></div>
                   </div>
--- resources/views/pages/user-form.blade.php
+++ resources/views/pages/user-form.blade.php
@@ -3,7 +3,7 @@
 <div class="">
             <div class="page-title">
               <div class="title_left">
-                <h3>Add Post</h3>
+                <h3></h3>
               </div>
 
               </div>
@@ -12,7 +12,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Form Design <small>different form elements</small></h2>
+                    <h2>Add Post </h2>
                     
                     <div class="clearfix"></div>
                   </div>
